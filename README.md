99719b58df6ee9f4ecffca99d892e982a79c9e89
dangos-dev <jabesalberto02@gmail.com>
2025-01-01 21:50:37 -0400

    🍡

---
+++ README.md
@@ -0,0 +1,28 @@
+# Telegram Bot with Webhooks Template
+
+![Python](https://img.shields.io/badge/python-3670A0?style=for-the-badge&logo=python&logoColor=ffdd54)
+![FastAPI](https://img.shields.io/badge/FastAPI-005571?style=for-the-badge&logo=fastapi)
+![Telegram](https://img.shields.io/badge/Telegram-2CA5E0?style=for-the-badge&logo=telegram&logoColor=white)
+![PyCharm](https://img.shields.io/badge/pycharm-143?style=for-the-badge&logo=pycharm&logoColor=black&color=black&labelColor=green)
+
+This template provides a basic framework for creating a Telegram bot using FastAPI, connecting it to Telegram's Webhook
+system.
+
+[![Deploy on Railway](https://railway.com/button.svg)](https://railway.com/template/5kprwG?referralCode=Al2B-n)
+
+## ✨ Features
+
+- **FastAPI**: Lightweight, fast web framework for building APIs.
+- **[Python Telegram Bot](https://python-telegram-bot.org/)**: Seamless integration with Telegram's Bot API.
+- **Webhook Support**: Automatic webhook setup and processing of Telegram updates using the `telegram.ext.Application`.
+
+## 💁‍♀️ How to install
+
+- Create a new repository from this template: Click
+  the [Use this template](https://github.com/new?template_name=TelegramBot.Webhook&template_owner=dangos-dev) button on
+  this repository's main page (or clone the repository).
+- Install packages with pip using `pip install -r requirements.txt`
+- Run locally using `hypercorn main:app --reload`
+
+## 🤖 Example
+Talk to [DangoBot - Telegram Webhooks](https://t.me/dango_webhook_bot) on Telegram
---
+++ main.py
@@ -0,0 +1,56 @@
+import os
+from contextlib import asynccontextmanager
+from http import HTTPStatus
+from dotenv import load_dotenv
+from fastapi import FastAPI, Request, Response
+from telegram import Update
+from telegram.ext import Application, ContextTypes, CommandHandler, MessageHandler, filters
+
+# Load environment variables
+load_dotenv()
+TELEGRAM_BOT_TOKEN: str = os.getenv('TELEGRAM_BOT_TOKEN')
+WEBHOOK_DOMAIN: str = os.getenv('RAILWAY_PUBLIC_DOMAIN')
+
+# Build the Telegram Bot application
+bot_builder = (
+    Application.builder()
+    .token(TELEGRAM_BOT_TOKEN)
+    .updater(None)
+    .build()
+)
+
+
+@asynccontextmanager
+async def lifespan(_: FastAPI):
+    """ Sets the webhook for the Telegram Bot and manages its lifecycle (start/stop). """
+    await bot_builder.bot.setWebhook(url=WEBHOOK_DOMAIN)
+    async with bot_builder:
+        await bot_builder.start()
+        yield
+        await bot_builder.stop()
+
+
+app = FastAPI(lifespan=lifespan)
+
+
+@app.post("/")
+async def process_update(request: Request):
+    """ Handles incoming Telegram updates and processes them with the bot. """
+    message = await request.json()
+    update = Update.de_json(data=message, bot=bot_builder.bot)
+    await bot_builder.process_update(update)
+    return Response(status_code=HTTPStatus.OK)
+
+
+async def start(update: Update, _: ContextTypes.DEFAULT_TYPE):
+    """ Handles the /start command by sending a "Hello world!" message in response. """
+    await update.message.reply_text("Hello! 🍡 Send me a message and I'll echo it back to you")
+
+
+async def echo(update: Update, _: ContextTypes.DEFAULT_TYPE) -> None:
+    """Echo the user message."""
+    await update.message.reply_text(update.message.text)
+
+
+bot_builder.add_handler(CommandHandler(command="start", callback=start))
+bot_builder.add_handler(MessageHandler(filters=filters.TEXT & ~filters.COMMAND, callback=echo))
